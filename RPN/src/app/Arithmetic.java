50cffb379e41c215078def21f3eede4d31265665
Jose Hernandez <josrhernandez1985@gmail.com>
2020-09-01 00:19:43 -0400

    Working on RPN

--- RPN/src/app/Arithmetic.java
+++ RPN/src/app/Arithmetic.java
@@ -13,16 +13,26 @@ public class Arithmetic {
         isBalanced(inFix);       
     }
     
-    public static boolean isBalanced(String inFix){
+    public boolean isBalanced(String inFix){
         
         boolean isBalanced = false;
         
-        Stack stack = new Stack();
+        Stack<Character> stack = new Stack<>();
         
         for(int i=0; i < inFix.length(); i++){
             char exp = inFix.charAt(i);
-
+            
+            if(exp == '('){
+                stack.push(exp);
+            }else if(exp == ')' && !stack.isEmpty()) {
+                stack.pop();
+            }          
         }
+        
+        isBalanced = stack.isEmpty();
+        return isBalanced;
     }
+        
+        
     
 }
--- RPN/src/app/RPN.java
+++ RPN/src/app/RPN.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Arrays;
+
 public class RPN {
 
     public static void main(String[] args) {
@@ -11,8 +13,14 @@ public class RPN {
                         "5 + (5 * 10 + (15 - 20) - 25) * 9"
                         };
         
-        for (int i = 0; i < s.length; i++){
+        for(int i = 0; i < s.length; i++){
+            Arithmetic a = new Arithmetic(s[i]);
             
+            if(a.isBalanced(s[i])){
+                System.out.println("Expression " + s[i] + " is Balanced\n");
+            }else{
+                System.out.println("Expression is not Balanced\n");
+            }
         }
         
     }
