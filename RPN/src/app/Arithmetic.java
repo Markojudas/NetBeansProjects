d48f4ae724c903169714ad7bad44f6a57271fe0d
Jose Hernandez <josrhernandez1985@gmail.com>
2020-09-02 00:02:42 -0400

    working on RPN/postFixExpression

--- RPN/src/app/Arithmetic.java
+++ RPN/src/app/Arithmetic.java
@@ -1,5 +1,5 @@
-
 package app;
+//Arithmetic class where the 3 methods are implemented 
 
 import java.util.Stack;
 
@@ -7,21 +7,28 @@ public class Arithmetic {
     
     public String inFix;
     
+    //constructor that calls the isBalanced() method to determine
+    // if the paranthesis are balanced
     public Arithmetic(String inFix){
         this.inFix = inFix;
-        
-        isBalanced(inFix);       
+              
     }
     
     public boolean isBalanced(String inFix){
         
         boolean isBalanced = false;
         
+        //Creating a stack to help with checking whether it is balanced or not
         Stack<Character> stack = new Stack<>();
         
+        //reading each string one character at a time from left to right
         for(int i=0; i < inFix.length(); i++){
             char exp = inFix.charAt(i);
             
+            //if statements to test the characters
+            //it will push onto the stack a left paranthesis "(" 
+            //it will then pop the stack once a right parenthesis ")" is encountered
+            //it will determine whether the expression is properly formed.
             if(exp == '('){
                 stack.push(exp);
             }else if(exp == ')' && !stack.isEmpty()) {
@@ -32,6 +39,25 @@ public class Arithmetic {
             }          
         }
         return isBalanced;
+    }
+    
+    public String postFixExpression(String inFix){
+        Stack<Character> charStack = new Stack<>();
+        String postFix = "";
+        
+        for(int i=0; i < inFix.length(); i++){
+            char exp = inFix.charAt(i);
+            
+            if(Character.isDigit(exp)){
+                postFix += exp;
+            }else if(exp == '('){
+                charStack.push(exp);
+            }else if(exp == ')' && !charStack.isEmpty()){
+                while(charStack.pop() != '(')
+                postFix += charStack.pop();
+            }
+        }
+        return postFix;
     }
         
         
--- RPN/src/app/RPN.java
+++ RPN/src/app/RPN.java
@@ -1,30 +1,38 @@
 package app;
-
-import java.util.Arrays;
-
+/*
+This program will use the concept of stack.
+It will first determine if a given arithmatic expression is properly formed
+Then will convert the infixed expression to postfix
+Finally it will then evalutate the postfixed expression and print the results
+*/
 public class RPN {
 
     public static void main(String[] args) {
         
+        //Array of Strings storing the given arithmetic expression
         String s[] =    {"5 + ) * ( 2",
                         "2 + ( - 3 * 5)",
                         "( ( 2 + 3 ) * 5 ) * 8",
                         "5 * 10 + ( 15 - 20) ) - 25",
                         "5 + ( 5 * 10 + ( 15 - 20 ) - 25 ) * 9"
                         };
-        
+        //declaring & initiating a counter for output purposes
         int counter =0;
         
+        //traversing through the array to start evaluating each String
+        //creating an instance of the Arithmetic class for each String
         for(int i = 0; i < s.length; i++){
             Arithmetic a = new Arithmetic(s[i]);
             counter++;
             
             if(a.isBalanced(s[i])){
-                System.out.println(counter + ". Expression " + s[i] + " is Balanced\n");
+                System.out.println(counter + ". Expression " + s[i] + " is Balanced");
+                System.out.println("\tThe postfix expression thus far is: " + a.postFixExpression(s[i]) + "\n");
             }else{
                 System.out.println(counter + ". Expression is not Balanced\n");
             }
         }
+        
         
     }
     
