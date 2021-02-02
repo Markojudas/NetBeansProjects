8e3ab3aa10617c738cc055c524ea38331fb1c7b4
Jose Hernandez <josrhernandez1985@gmail.com>
2021-02-01 21:47:56 -0500

    ChatServer updated : accepts DMs

--- ChatServer/src/chatserver/ServerUser.java
+++ ChatServer/src/chatserver/ServerUser.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.io.PrintWriter;
 
 
 /**
@@ -26,6 +27,7 @@ public class ServerUser implements Runnable{
     public final Server server;
     private String login = null;
     private OutputStream outputStream;
+    private PrintWriter out;
     private final List<ServerUser> userList;
     private final Lock chatLock;
     private final Condition chatReady;
@@ -55,12 +57,21 @@ public class ServerUser implements Runnable{
         InputStream inputStream = userSocket.getInputStream();
         outputStream = userSocket.getOutputStream();
         
+        out = new PrintWriter(outputStream);
+        
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         
-        outputStream.write("\n========== WELCOME TO THE ULTIMATE CHAT ROOM ==========\n\n".getBytes());
-        outputStream.write("Please choose a username: #login <username> (4 - 10 characters)\n".getBytes());
-               
+        //outputStream.write("\n========== WELCOME TO THE ULTIMATE CHAT ROOM ==========\n\n".getBytes());
+        //outputStream.write("PLEASE CHOOSE A USERNAME: #login <username> (4 - 10 characters)\n".getBytes());
+        
+        String welcomeMsg1 = "\n========== WELCOME TO THE ULTIMATE CHAT ROOM ==========\n";
+        String welcomeMsg2 = "PLEASE CHOOSE A USERNAME: #login <username> (4 - 10 characters)";
+        
+        out.println(welcomeMsg1);
+        out.println(welcomeMsg2);
+        out.flush();
+        
             while((line = reader.readLine()) != null){
                 String[] tokens = line.split(" ");
                 if (tokens != null && tokens.length > 0){
@@ -71,41 +82,71 @@ public class ServerUser implements Runnable{
                         break;
                     }
                     else if("#login".equalsIgnoreCase(cmd)){
-                        chatLock.lock();
-                        try{
-                            List<ServerUser> users = new ArrayList<>();
+                        if(login == null){
+                            chatLock.lock();
+                            try{
+                                List<ServerUser> users = new ArrayList<>();
  
-                            if(userList.size() >= 1){
-                                boolean nameIsTaken = false;
-                                for(int i = 0; i < userList.size(); i++){
-                                    ServerUser user = userList.get(i);
-                                    users.add(user);
-                                }                            
-                                for(ServerUser userName : users){
-                                    if(tokens[1].equalsIgnoreCase(userName.getLogin())){
-                                        outputStream.write("\nUSERNAME TAKEN, PLEASE TRY AGAIN\n\n".getBytes());
-                                        nameIsTaken = true;
-                                        break;
+                                if(userList.size() >= 1){
+                                    boolean nameIsTaken = false;
+                                    for(int i = 0; i < userList.size(); i++){
+                                        ServerUser user = userList.get(i);
+                                        users.add(user);
+                                    }                            
+                                    for(ServerUser userName : users){
+                                        if(tokens[1].equalsIgnoreCase(userName.getLogin())){
+                                            //outputStream.write("\nUSERNAME TAKEN, PLEASE TRY AGAIN\n\n".getBytes());
+                                            out.println("\nUSERNAME TAKEN, PLEASE TRY AGAIN\n");
+                                            out.flush();
+                                            nameIsTaken = true;
+                                            break;
+                                        }
                                     }
+                                    if(!nameIsTaken){
+                                        doLogin(out, tokens);
+                                    }
+                                }else{
+                                    doLogin(out, tokens);                            
                                 }
-                                if(!nameIsTaken){
-                                    doLogin(outputStream, tokens);
-                                }
-                            }else{
-                                doLogin(outputStream, tokens);                            
+                                chatReady.signalAll();
+                            }finally{
+                                chatLock.unlock();
                             }
-                            chatReady.signalAll();
-                        }finally{
-                            chatLock.unlock();
+                        }else{
+                            out.println("\nYOU ARE ALREADY SIGNED IN\n");
+                            out.flush();
                         }
                     }    
                     else if("#status".equalsIgnoreCase(cmd)){
-                        showUsers(outputStream);
-                        outputStream.write("\n".getBytes());
+                        if(login != null){
+                            showUsers(out);
+                            out.println();
+                            out.flush();
+                            //outputStream.write("\n".getBytes());                
+                        }else{
+                            //outputStream.write("PLEASE LOGIN IN FIRST\n".getBytes());
+                            out.println("\nPLEASE LOGIN FIRST");
+                            out.println();
+                            out.flush();
+                        }
+                    }   
+                    else if("#dm".equalsIgnoreCase(cmd)){
+                        if(login != null){
+                            sendDM(tokens);
+                        }else{
+                            out.println("\nPLEASE LOGIN FIRST");
+                            out.flush();
+                        }    
                     }
                     else{
-                        String msg = login + ": " + line + "\n";
-                        sendMsg(msg);
+                        if(login != null){
+                            String msg = login + ": " + line;
+                            sendMsg(msg);
+                        }else{
+                            //outputStream.write("PLEASE LOGIN IN FIRST\n".getBytes());
+                            out.println("\nPLEASE LOGIN FIRST");
+                            out.flush();
+                        }     
                     }
                 }    
             }       
@@ -116,30 +157,40 @@ public class ServerUser implements Runnable{
         return login;
     }
     
-    public void sendMsg(String msg) throws IOException{
-        if(login != null){            
-            for(ServerUser worker : userList){
-                worker.send(msg);
-            }          
+    public void sendMsg(String msg) throws IOException{           
+        for(ServerUser worker : userList){
+            worker.send(msg);
         }               
     }
     
-    public void showUsers(OutputStream outputStream) throws IOException{
+    public void showUsers(PrintWriter out) throws IOException{
         
-        outputStream.write("\nUSERS CURRENTLY ONLINE: \n".getBytes());
+        //outputStream.write("\nUSERS CURRENTLY ONLINE: \n".getBytes());
+        out.println("\nUSERS CURRENTLY ONLINE: ");
         for(ServerUser worker : userList){
-            outputStream.write((worker.getLogin() + "\n").getBytes());
+            //outputStream.write((worker.getLogin() + "\n").getBytes());
+            out.println(worker.getLogin());
         }
+        out.flush();
     }
     
-    private void doLogin(OutputStream outputStream, String[] tokens) throws IOException{
+    private void doLogin(PrintWriter out, String[] tokens) throws IOException{
 
         if(tokens.length == 2){
             String login = tokens[1];
             
             if ((login.length() >= 4) && (login.length() <= 10)){
-                String msg = "\nLOGIN SUCESSFUL!!! YOU CAN NOW PARTICIPATE\n" + "TO DISCONNECT: #logoff\n" + "TO CHECK ONLINE USERS: #status\n\n";
-                outputStream.write(msg.getBytes());
+                //String msg = "\nLOGIN SUCESSFUL!!! YOU CAN NOW PARTICIPATE\nTO DISCONNECT: #logoff\nTO CHECK ONLINE USERS: #status\n\n";
+                //outputStream.write(msg.getBytes());
+                String msg1 = "LOGIN SUCCESSFUL!!! YOU CAN NOW PARTICIPATE!";
+                String msg2 = "TO DISCONNECT: #logoff";
+                String msg3 = "TO CHECK ONLINE USERS: #status";
+                out.println();
+                out.println(msg1);
+                out.println(msg2);
+                out.println(msg3);
+                out.println();
+                out.flush();
                 chatLock.lock();
                 try{
                     userList.add(this);
@@ -152,7 +203,7 @@ public class ServerUser implements Runnable{
                 System.out.println("User logged in: " + login);
 
                 //send other online users current user's status
-                String onlineMsg = login + " HAS COME ONLINE\n\n";
+                String onlineMsg = login + " HAS COME ONLINE\n";
                 for(ServerUser worker : userList){
                     
                     if(!login.equals(worker.getLogin())){
@@ -162,14 +213,18 @@ public class ServerUser implements Runnable{
             }
             else {
                 String msg = "ERROR LOGIN\n";
-                outputStream.write(msg.getBytes());
+                //outputStream.write(msg.getBytes());
+                out.println(msg);
+                out.flush();
             }
         }       
     }
     
     public void send(String msg) throws IOException{
         if(login != null){
-            outputStream.write(msg.getBytes());
+            //outputStream.write(msg.getBytes());
+            out.println(msg);
+            out.flush();
         }
     }
 
@@ -184,16 +239,60 @@ public class ServerUser implements Runnable{
             String onlineMsg = login + " HAS DISCONNECTED\n";
             System.out.println("User logged off: " + login);
             
-            for(ServerUser worker : onlineUsers){
+            for(ServerUser user : onlineUsers){
                     
-                if(!login.equals(worker.getLogin())){
-                    worker.send(onlineMsg);
+                if(!login.equals(user.getLogin())){
+                    user.send(onlineMsg);
                 } 
             }
-            userSocket.close();
             chatReady.signalAll();
         }finally{
             chatLock.unlock();
-        }    
-    }            
+        }
+        userSocket.close();
+    }
+        
+    private void sendDM(String[] tokens) throws IOException {
+        
+        if(tokens.length >=3){
+            String sendTo = tokens[1];
+            String msg = "";
+            boolean userFound = false;
+            
+            for(int i = 2; i < tokens.length; i++){
+                msg += tokens[i] + " ";
+            }
+            
+            chatLock.lock();
+            try{
+                List<ServerUser> onlineUsers = userList;
+                
+                int endOfList = 0;
+                for(ServerUser user : onlineUsers){
+                    endOfList++;
+                    if(sendTo.equalsIgnoreCase(user.getLogin())){
+                        userFound = true;
+                        if(userFound){
+                            String completeMsg = "DM from " + login + ": " + msg;
+                            user.send(completeMsg);
+                            out.println("DM sent to " + sendTo + ": " + msg);
+                            out.flush();
+                        }
+                    }else{
+                        if((!userFound) && endOfList == onlineUsers.size()){
+                            out.println("\nUSER NOT FOUND\n");
+                            out.flush();                            
+                        }    
+                    }                    
+                }
+                chatReady.signalAll();
+            }
+            finally{
+                chatLock.unlock();
+            }
+            
+        }
+        
+    }
+    
 }
